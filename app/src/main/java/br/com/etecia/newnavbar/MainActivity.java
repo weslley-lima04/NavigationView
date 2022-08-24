b01d1b78bd09684b34353660a952375e24246122
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-08-23 22:16:13 -0300

    dando problema com id

--- app/src/main/java/br/com/etecia/newnavbar/MainActivity.java
+++ app/src/main/java/br/com/etecia/newnavbar/MainActivity.java
@@ -1,6 +1,7 @@
 package br.com.etecia.newnavbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ import com.google.android.material.appbar.MaterialToolbar;
 public class MainActivity extends AppCompatActivity {
 
     MaterialToolbar toolbar;
+    DrawerLayout drawerLayout;
 
 
     @Override
@@ -18,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.topAppBar);
+
+
+
         toolbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
