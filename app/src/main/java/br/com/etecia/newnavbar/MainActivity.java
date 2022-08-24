088062bb9a8d29986c54fe0fadd016601a4c8f19
weslley-lima04 <weslley.lima24@etec.sp.gov.br>
2022-08-23 22:01:42 -0300

    Iniciamos o java

--- app/src/main/java/br/com/etecia/newnavbar/MainActivity.java
+++ app/src/main/java/br/com/etecia/newnavbar/MainActivity.java
@@ -3,12 +3,32 @@ package br.com.etecia.newnavbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialToolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.topAppBar);
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+       // toolbar.setNavigationIcon();
+
+
+        //toolbar.setOnMenuItemClickListener();
+
     }
 }
